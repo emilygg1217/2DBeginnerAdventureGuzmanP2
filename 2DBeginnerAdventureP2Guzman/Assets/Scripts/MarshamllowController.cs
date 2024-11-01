1acd115394970d8520dec7180503629064dbe14b
emilygg1217 <emilyg1217@icloud.com>
2024-11-01 09:58:00 -0700

    worked on my bveginner desing tile map
    i am on video world design tile maps, 13:31.

--- 2DBeginnerAdventureP2Guzman/Assets/Scripts/MarshamllowController.cs
+++ 2DBeginnerAdventureP2Guzman/Assets/Scripts/MarshamllowController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class MarshmallowConsole : MonoBehaviour
+public class MarshmallowController: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +14,12 @@ public class MarshmallowConsole : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
